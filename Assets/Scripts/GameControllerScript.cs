c6467c493be821353b669a5df550ebb33cdc9c92
ShunsukeYamamoto <59650988+ShunsukeYamamoto@users.noreply.github.com>
2020-03-24 15:11:45 +0900

    穴をInstantiateで6個作成
    - GameControllerScriptでプレハブ化した穴を6個start()で生成

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class GameControllerScript : MonoBehaviour
 {
+    public GameObject mogura;
+    public GameObject hole;
+    float space = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int x = 0; x < 3; x++)
+        {
+            for(int y = 0; y < 2; y++)
+            {
+                Instantiate(
+                    hole,
+                    new Vector3(-4f + space * x, -2.5f * y, 0f),
+                    transform.rotation);
+
+            }
+        }
     }
 
     // Update is called once per frame
