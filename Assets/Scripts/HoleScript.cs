90cc95ab057b1e6f3ddcf160d29508a98254b262
ShunsukeYamamoto <59650988+ShunsukeYamamoto@users.noreply.github.com>
2020-03-25 12:03:20 +0900

    モグラアニメーション化
    - モグラに0.5sかけて出現、3s維持、0.5sかけて消えるアニメーションを付与

--- Assets/Scripts/HoleScript.cs
+++ Assets/Scripts/HoleScript.cs
@@ -13,27 +13,12 @@ public class HoleScript : MonoBehaviour
             new Vector3(transform.position.x, transform.position.y + 1, 0f),
             transform.rotation
             );
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    IEnumerator SpawnMogura()
-    {
-        Instantiate(
-            mogura,
-            new Vector3(transform.position.x, transform.position.y + 1, 0f),
-            transform.rotation
-            );
-        yield return new WaitForSeconds(1.0f);
-    }
-
-    IEnumerator DeleteMogura()
-    {
-        Destroy(mogura);
-        yield return new WaitForSeconds(1.0f);
     }
 }
--- Assets/Scripts/MoguraScript.cs
+++ Assets/Scripts/MoguraScript.cs
@@ -7,12 +7,16 @@ public class MoguraScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine("SpawnMogura");
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnMouseDown();
+        }
     }
 
     void OnMouseDown()
